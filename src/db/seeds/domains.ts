82c63d26e8ae30e533d3b743c32d15c7c418ba4c
Philip <philip_angelin@hotmail.com>
2021-10-10 15:39:27 +0200

    Bloat comments

--- src/db/seeds/domains.ts
+++ src/db/seeds/domains.ts
@@ -1,10 +1,8 @@
 import { Knex } from "knex";
 
 export async function seed(knex: Knex) {
-    // Deletes ALL existing entries
     await knex("domains").del();
 
-    // Inserts seed entries
     await knex("domains").insert([
         { name: "The Pioneer Hangout", url: "https://tph.angelin.dev" },
         { name: "Blog", url: "https://blog.angelin.dev" },
