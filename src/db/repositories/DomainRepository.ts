b83e876b0ba78ac94dd726a9af8d5f88a00f858a
Philip <philip@angelin.dev>
2021-10-11 10:15:01 +0200

    Removed log

--- src/db/repositories/DomainRepository.ts
+++ src/db/repositories/DomainRepository.ts
@@ -4,7 +4,6 @@ import { Domain } from "@db/models/Domain";
 @injectable()
 export class DomainRepository {
     public getAll() {
-        console.log(Domain.query());
         return Domain.query();
     }
 }
