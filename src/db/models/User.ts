064bf2c28740a26d0efc9b2ee99ed7e2b2c0caac
Philip <philip@angelin.dev>
2021-10-14 16:02:45 +0200

    Added start of user

--- src/db/models/User.ts
+++ src/db/models/User.ts
@@ -1,21 +1,19 @@
 import { Model, JSONSchema } from "objection";
 
-export class Domain extends Model {
-    public static readonly tableName = "domains";
+export class User extends Model {
+    public static readonly tableName = "users";
 
     public id: string;
-    public name: string;
-    public url: string;
+    public emails: string;
     public created_at: string;
     public updated_at: string;
 
     public static jsonSchema: JSONSchema = {
         type: "object",
-        required: ["id", "url", "name", "created_at", "updated_at"],
+        required: ["id", "email", "created_at", "updated_at"],
         properties: {
             id: { type: ["string", "integer"] },
-            url: { type: "string", minLength: 1 },
-            name: { type: "string", minLength: 1 },
+            email: { type: "string", minLength: 1 },
             created_at: { type: "string", minLength: 1 },
             updated_at: { type: "string", minLength: 1 },
         },
--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,7 +1,11 @@
 export {};
 
+import { User as UserModel } from "@db/models";
+
 declare global {
     namespace Express {
+        interface User extends UserModel {}
+
         interface Response {
             expectsJSON?: boolean;
         }
