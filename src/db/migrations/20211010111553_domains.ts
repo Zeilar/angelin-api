12d2e0221ce3755aad1a7497a298e0df82116b26
Philip <philip@angelin.dev>
2021-10-18 16:48:49 +0200

    Added users to migrations/seeds

--- src/db/migrations/20211010111553_domains.ts
+++ src/db/migrations/20211010111553_domains.ts
@@ -1,19 +1,33 @@
 import { Knex } from "knex";
 
 export async function up(knex: Knex) {
-    return knex.schema.createTable("domains", (table) => {
-        table.increments();
-        table.string("name").unique().notNullable();
-        table.string("url").unique().notNullable();
-        table
-            .timestamp("created_at")
-            .notNullable()
-            .defaultTo(knex.raw("CURRENT_TIMESTAMP"));
-        table
-            .timestamp("updated_at")
-            .notNullable()
-            .defaultTo(knex.raw("CURRENT_TIMESTAMP"));
-    });
+    return knex.schema
+        .createTable("domains", (table) => {
+            table.increments();
+            table.string("name").unique().notNullable();
+            table.string("url").unique().notNullable();
+            table
+                .timestamp("created_at")
+                .notNullable()
+                .defaultTo(knex.raw("CURRENT_TIMESTAMP"));
+            table
+                .timestamp("updated_at")
+                .notNullable()
+                .defaultTo(knex.raw("CURRENT_TIMESTAMP"));
+        })
+        .createTable("users", (table) => {
+            table.increments();
+            table.string("email").unique().notNullable();
+            table.string("password").nullable();
+            table
+                .timestamp("created_at")
+                .notNullable()
+                .defaultTo(knex.raw("CURRENT_TIMESTAMP"));
+            table
+                .timestamp("updated_at")
+                .notNullable()
+                .defaultTo(knex.raw("CURRENT_TIMESTAMP"));
+        });
 }
 
 export async function down(knex: Knex) {
--- src/db/seeds/domains.ts
+++ src/db/seeds/domains.ts
@@ -1,8 +1,12 @@
 import { Knex } from "knex";
 
 export async function seed(knex: Knex) {
-    await knex("domains").del();
+    await knex("users").del();
+    await knex("users").insert([
+        { email: "philip@angelin.dev", url: "https://tph.angelin.dev" },
+    ]);
 
+    await knex("domains").del();
     await knex("domains").insert([
         { name: "The Pioneer Hangout", url: "https://tph.angelin.dev" },
         { name: "Blog", url: "https://blog.angelin.dev" },
